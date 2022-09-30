1378256af34423beaed1ed79bb758605dedf6fc7
aito <BoxPistols@users.noreply.github.com>
2022-09-30 22:46:10 +0900

    set children on Layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,15 @@
-import Link from "next/link"
+// import Link from "next/link"
 import React from "react"
+import { LayoutProps } from "../types/types"
 import { Footer } from "./Footer"
 import { NavBar } from "./NavBar"
 
-
-const Layout = () => {
+const Layout = ({ children }: LayoutProps) => {
   return (
     <div className='relative overflow-hidden'>
       <div className='flex flex-col items-center w-full max-w-2xl mx-auto'>
         <NavBar />
-        <main></main>
+        <main className='w-full px-4 pb-12'>{children}</main>
         <Footer />
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,17 @@ import Head from "next/head"
 import Image from "next/image"
 import Link from "next/link"
 import Layout from "../components/Layout"
+import { siteConfig } from "../site.config"
 
 const Home: NextPage = () => {
-  return <Layout />
+  return (
+    <Layout>
+      <div className='pt-12'>
+        <div className='grid w-full my-12 mt-10 md:gap-6 md:grid-cols-2'>
+          <h1 className='mb-8 text-5xl'>{siteConfig.title}</h1>
+        </div>
+      </div>
+    </Layout>
+  )
 }
-
 export default Home
