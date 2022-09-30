7a4263d830b4afcd4fdffb16cfb17de6b5f32036
aito <BoxPistols@users.noreply.github.com>
2022-10-01 01:24:38 +0900

    add mock-api / main-height

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,11 +5,17 @@ import { Footer } from "./Footer"
 import { NavBar } from "./NavBar"
 
 const Layout = ({ children }: LayoutProps) => {
+  const navFooter = 128 + 48
   return (
     <div className='relative overflow-hidden'>
       <div className='flex flex-col items-center w-full max-w-2xl mx-auto'>
         <NavBar />
-        <main className='w-full px-4 pb-12'>{children}</main>
+        <main
+          className='w-full min-h-full px-4 pb-12'
+          style={{ height: `calc((100vh - (${navFooter}px))` }}
+        >
+          {children}
+        </main>
         <Footer />
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,18 @@ import Image from "next/image"
 import Link from "next/link"
 import Layout from "../components/Layout"
 import { siteConfig } from "../site.config"
+import { sampleCards } from "../utils/sample"
 
 const Home: NextPage = () => {
   return (
     <Layout>
       <div className='pt-12'>
+        <h1 className='mb-8 text-5xl'>{siteConfig.title}</h1>
         <div className='grid w-full my-12 mt-10 md:gap-6 md:grid-cols-2'>
-          <h1 className='mb-8 text-5xl'>{siteConfig.title}</h1>
+          {/* Card */}
+          {sampleCards.map((page, index) => (
+            <p key={index}>{page.name}</p>
+          ))}{" "}
         </div>
       </div>
     </Layout>
