66a760ad3fd6bcd1f53eb486a559b85c76dc080f
aito <BoxPistols@users.noreply.github.com>
2022-10-02 04:49:29 +0900

    add articles / meta

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["placeimg.com", "placekitten.com"],
+    domains: ["placeimg.com", "placekitten.com", "placehold.jp"],
   },
 }
 
