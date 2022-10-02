58b44760ac1d835642b68bd000be16f757ee7665
aito <BoxPistols@users.noreply.github.com>
2022-10-02 22:01:42 +0900

    set slug as page

--- components/ArticleMeta.tsx
+++ components/ArticleMeta.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import React from "react"
 
-const ArticleMeta = () => {
+const ArticleMeta = ({ page }) => {
   return (
     <>
       {/* page cover */}
@@ -9,7 +9,7 @@ const ArticleMeta = () => {
       {undefined! && (
         <Image
           className='w-full max-w-screen-lg my-4 rounded-lg aspect-video'
-          src={"/"}
+          src={page.cover}
           alt=''
           objectFit='cover'
           width={640}
@@ -19,22 +19,24 @@ const ArticleMeta = () => {
       )}
 
       {/* page name */}
-      <h1 className='my-8'>page name</h1>
+      <h1 className='my-8'>{page.name}</h1>
       <div className='px-6 py-4 text-sm text-gray-500 bg-gray-100 rounded'>
         <div className='grid grid-cols-3 gap-4'>
           {/* published */}
           <div className='col-span-1'>Published</div>
-          <div className='col-span-2'>published</div>
+          <div className='col-span-2'>{page.published}</div>
 
           {/* author */}
           <div className='col-span-1'>Author</div>
-          <div className='col-span-2'>author</div>
+          <div className='col-span-2'>{page.author}</div>
 
           {/* tags */}
           <div className='col-span-1'>Tags</div>
           <div className='col-span-2'>
             {/* change later */}
-            <span>#tag</span>
+            {page.tags.map((tag: string, index: number) => (
+              <span key={index}>{`#${tag} `}</span>
+            ))}
           </div>
         </div>
       </div>
