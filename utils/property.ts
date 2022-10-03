9a306d394e54031ebe7ef9fe45efa94161951376
aito <BoxPistols@users.noreply.github.com>
2022-10-04 04:21:04 +0900

    get some function

--- utils/property.ts
+++ utils/property.ts
@@ -1,11 +1,35 @@
-import { RichTextType } from "../types/types"
+import { PageType, RichTextType } from "../types/types"
 
 export const getText = (richTextArr: RichTextType[]) => {
   try {
-    const texArr = richTextArr.map((richText) => richText.plain_text)
-    return texArr.join("")
-  } catch (e) {
-    console.log(e)
+    const textArr = richTextArr.map((richText) => richText.plain_text)
+    return textArr.join("")
+  } catch (err) {
+    // console.error(err)
   }
   return ""
+}
+
+export const getCover = (cover: PageType["cover"]) => {
+  if (cover && cover.file) return cover.file.url
+  if (cover && cover.external) return cover.external.url
+  return "http://placekitten.com/800/600"
+}
+
+export const getDate = (date: { start: string }) => {
+  try {
+    return date.start
+  } catch (err) {
+    // console.error(err)
+  }
+  return "-"
+}
+
+export const getMultiSelect = (multiSelect: [{ name: string }]) => {
+  try {
+    return multiSelect.map((tag) => tag.name)
+  } catch (err) {
+    // console.error(err)
+  }
+  return []
 }
