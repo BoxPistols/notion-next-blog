17f295197aaba9d25178589cdea4516a4e924372
aito <BoxPistols@users.noreply.github.com>
2022-10-10 22:29:55 +0900

    set slug ID

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -1,23 +1,30 @@
+import { GetServerSideProps, GetStaticProps, NextPage } from "next"
 import Image from "next/image"
-import { GetServerSideProps } from "next/types"
 import React from "react"
 import ArticleMeta from "../../components/ArticleMeta"
 import Layout from "../../components/Layout"
 import { ArticleProps, Params } from "../../types/types"
+import { fetchBlocksByPageId, fetchPages } from "../../utils/notion"
 import { sampleCards } from "../../utils/sample"
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
+export const getStaticProps: GetStaticProps = async (ctx) => {
   const { slug } = ctx.params as Params
 
-  const page = sampleCards.find((data) => data.slug === slug)
+  const { results } = await fetchPages({ slug: slug })
+  const page = results[0]
+  const pageId = page.id
+  const { results: blocks } = await fetchBlocksByPageId(pageId)
 
   return {
-    // props: { slug: slug },
-    props: { page: page },
+    props: {
+      page: page,
+      blocks: blocks,
+    },
+    revalidate: 10,
   }
 }
 
-const Article = ({ page }: ArticleProps) => {
+const Article: NextPage<ArticleProps> = ({ page }) => {
   return (
     <Layout>
       <article className='w-full'>
